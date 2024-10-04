da507a883b9815aea326a5b8017a65c9c69a9fa2
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-10-04 07:04:47 +0500

    My Portfolio

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React from 'react';
@@ -7,12 +8,11 @@ import Image from 'next/image';
 const HeroSection: React.FC = () => {
   return (
     <section
-    className="flex flex-col lg:flex-row items-center justify-center py-16 min-h-[70vh] bg-no-repeat bg-cover bg-center px-5 lg:p-10 background"
-    style={{
-      boxShadow: '0 8px 10px rgba(0, 0, 0, 0.2)',
-    }}
-  >
-      
+      className="flex flex-col lg:flex-row items-center justify-center py-16 min-h-[70vh] bg-no-repeat bg-cover bg-center px-5 lg:p-10 background"
+      style={{
+        boxShadow: '0 8px 10px rgba(0, 0, 0, 0.2)',
+      }}
+    >
       {/* Profile Picture */}
       <div className="relative lg:mb-0 lg:mr-40 mt-18">
         <Image
@@ -26,17 +26,16 @@ const HeroSection: React.FC = () => {
 
       {/* Text Section */}
       <div className="text-left text-[#002244] md:w-[800px] mt-10 mx-4 md:mt-0">
-        <p className=" text-lg md:text-2xl mt-2 font-semibold ">Hello,</p>
-        <h2 className=" text-3xl md:text-5xl font-extrabold " >
+        <p className="text-lg md:text-2xl mt-2 font-semibold">Hello,</p>
+        <h2 className="text-3xl md:text-5xl font-extrabold">
           I'm <span className="text-pink-400" style={{ textShadow: '2px 2px 2px #002244' }}>Yusra Saleem</span>
         </h2>
-        <p className=" text-lg md:text-2xl mt-2 font-medium ">
-        I am passionate about creating stunning web applications with a focus on responsive design, modern UI/UX, and clean code. Let's build something great together!
+        <p className="text-lg md:text-2xl mt-2 font-medium">
+          I am passionate about creating stunning web applications with a focus on responsive design, modern UI/UX, and clean code. Let&apos;s build something great together!
         </p>
-        <p className="text-lg leading-relaxed mb-8 max-w-lg mx-auto">
-        </p>
+        <p className="text-lg leading-relaxed mb-8 max-w-lg mx-auto"></p>
 
-        <h2 className=" text-2xl md:text-4xl font-bold text-indigo-800">
+        <h2 className="text-2xl md:text-4xl font-bold text-indigo-800">
           <Typewriter
             options={{
               strings: ["Web Designer.", "Frontend Developer.", "UI/UX Designer.", "Full-Stack Developer.", "Digital Marketer."],
@@ -60,4 +59,5 @@ const HeroSection: React.FC = () => {
 };
 
 export default HeroSection;
+
 
