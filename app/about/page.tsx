c9c845a89953b8537be54de18486eb120fd32b4d
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-10-04 07:11:58 +0500

    My Portfolio

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import 'animate.css/animate.min.css'; // Importing animate.css for animations
 import Image from 'next/image';
@@ -27,10 +28,10 @@ function About() {
       {/* Animated Text Section */}
       <div className="lg:w-1/2 w-full text-center lg:text-left mt-8 lg:mt-0 animate__animated animate__fadeInRight">
         <h2 className="text-2xl sm:text-3xl font-bold mb-4 text-[#133850] hover:text-sky-800 cursor-default">
-          Hi, I'm Yusra Saleem
+          Hi, I&apos;m Yusra Saleem
         </h2>
         <p className="text-sm sm:text-base lg:text-lg leading-relaxed font-medium mb-6 px-4 sm:px-8 lg:px-0">
-          I am a passionate and dedicated Full Stack Developer with a strong focus on front-end technologies. Over the years, I've honed my skills in <span className="text-[#e58f8b] font-bold">HTML</span>, <span className="text-[#e58f8b] font-bold">CSS</span>, <span className="text-[#e58f8b] font-bold">JavaScript</span>, and <span className="text-[#e58f8b] font-bold">TypeScript</span>. I've expanded my expertise to modern frameworks such as <span className="text-[#e58f8b] font-bold">Next.js</span>, always striving to stay up-to-date with the latest technologies.
+          I am a passionate and dedicated Full Stack Developer with a strong focus on front-end technologies. Over the years, I&apos;ve honed my skills in <span className="text-[#e58f8b] font-bold">HTML</span>, <span className="text-[#e58f8b] font-bold">CSS</span>, <span className="text-[#e58f8b] font-bold">JavaScript</span>, and <span className="text-[#e58f8b] font-bold">TypeScript</span>. I&apos;ve expanded my expertise to modern frameworks such as <span className="text-[#e58f8b] font-bold">Next.js</span>, always striving to stay up-to-date with the latest technologies.
         </p>
 
         {/* Animated Button */}
@@ -38,7 +39,7 @@ function About() {
           href="/contact" // Adjust the link as necessary
           className="inline-block bg-[#EA9A96] text-[#002244] py-2 px-6 sm:py-3 sm:px-8 rounded-full font-bold hover:bg-[#FFD700] transition-colors duration-300 animate__animated animate__pulse animate__infinite"
         >
-          Let's Connect
+          Let&apos;s Connect
         </Link>
       </div>
     </section>
@@ -46,3 +47,4 @@ function About() {
 }
 
 export default About;
+
