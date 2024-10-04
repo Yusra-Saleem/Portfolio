1217e4b38e6d253eba6aaa56c8116817466a0749
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-10-04 07:43:42 +0500

    Create react-slick.d.ts

---
+++ react-slick.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-slick' {
+  import { Component } from 'react';
+
+  interface SliderProps {
+    dots?: boolean;
+    infinite?: boolean;
+    speed?: number;
+    slidesToShow?: number;
+    slidesToScroll?: number;
+  }
+  
